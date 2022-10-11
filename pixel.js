9fc4c230d48ca2ccf2af4fb0a703375003dfdf1c
Michał Zaręba <michalzareba0001@gmail.com>
2022-10-11 22:28:43 +0200

    just black square for now ;p

--- pixel.js
+++ pixel.js
@@ -1,4 +1,42 @@
 window.addEventListener('load', function() { 
-    cnst
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    const image1 = document.getElementById('image1');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    class Particle {
+        constructor(){
+            this.x = 100;
+            this.y = 100;
+            this.size = 50;
+        }
+        draw(){
+            ctx.fillRect(this.x, this.y, this.size, this.size);
+        }
+
+    }
+
+    class Effect {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.particlesArray = [];
+        }
+        init(){
+            this.particlesArray.push(new Particle());
+        }
+
+
+    }
+
+    const particle1 = new Particle();
+    particle1.draw();
+
+    function animate() { 
+
+    }
+
 
 });
