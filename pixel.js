10e1b530dabe2f3e3700af48992e67f52b6bd496
Michał Zaręba <michalzareba0001@gmail.com>
2022-10-11 23:53:40 +0200

    it's alive!!!

--- pixel.js
+++ pixel.js
@@ -1,19 +1,25 @@
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    const image1 = document.getElementById('image1');
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     class Particle {
-        constructor() {
-            this.x = Math.random() * canvas.width;
-            this.y = Math.random() * canvas.height;
-            this.size = 50;
+        constructor(effect) {
+            this.effect = effect;
+            this.x = Math.random() * this.effect.width;
+            this.y = Math.random() * this.effect.height;
+            this.size = 10;
+            this.vx = Math.random() * 10 - 5;
+            this.vy = Math.random() * 10 - 5;
         }
         draw(context) {
             context.fillRect(this.x, this.y, this.size, this.size);
+        }
+        update() {
+            this.x += this.vx;
+            this.y += this.vy;
         }
 
     }
@@ -23,25 +29,43 @@ window.addEventListener('load', function () {
             this.width = width;
             this.height = height;
             this.particlesArray = [];
+            this.image = document.getElementById('image1');
+            this.centerx = this.width * 0.5;
+            this.centery = this.height * 0.5;
+            this.x = this.centerx - this.image.width * 0.5;
+            this.y = this.centery - this.image.height * 0.5;
         }
         init() {
-            this.particlesArray.push(new Particle());
+            for (let i = 0; i < 100; i++) {
+                this.particlesArray.push(new Particle(this));
+            }
+
+
         }
         draw(context) {
             this.particlesArray.forEach((particle) =>
                 particle.draw(context));
+            context.drawImage(this.image, this.x, this.y,);
+        }
+        update() {
+            this.particlesArray.forEach((particle) =>
+                particle.update());
         }
 
     }
 
     const effect = new Effect(canvas.width, canvas.height);
     effect.init();
-    effect.draw(ctx);
-    console.log('effect', effect);
+
 
     function animate() {
-
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        effect.draw(ctx);
+        effect.update();
+        requestAnimationFrame(animate);
     }
-
+    
+    animate();
 
 });
+
