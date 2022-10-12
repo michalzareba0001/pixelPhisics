4d3adddace32b0a6e003ecb51c672742eeb97df9
Michał Zaręba <michalzareba0001@gmail.com>
2022-10-12 21:23:23 +0200

    rect animation backup created

--- 1_Rect_animation/pixel.js
+++ 1_Rect_animation/pixel.js
@@ -6,16 +6,14 @@ window.addEventListener('load', function () {
     canvas.height = window.innerHeight;
 
     class Particle {
-        constructor(effect, x, y, color) {
+        constructor(effect) {
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
-            this.OriginX = Math.floor(x);
-            this.OriginY = Math.floor(y);
             this.size = Math.random() * 5 + 0.5;
             this.vx = Math.random() * 6 - 3;
             this.vy = Math.random() * 5 - 3;
-            this.color = color;
+            this.color = 'yellow';
         }
         draw(context) {
             context.fillRect(this.x, this.y, this.size, this.size);
@@ -39,33 +37,20 @@ window.addEventListener('load', function () {
             this.centery = this.height * 0.5;
             this.x = this.centerx - this.image.width * 0.5;
             this.y = this.centery - this.image.height * 0.5;
-            this.gap = 5;
         }
-        init(context) {
-            context.drawImage(this.image, this.x, this.y,);
-            const pixels = context.getImageData(0, 0, this.width, this.height).data;
-            for (let y = 0; y < this.height; y += this.gap) {
-                for (let x = 0; x < this.width; x += this.gap) {
-                    const index = (y * this.width + x) * 4;
-                    const red = pixels[index];
-                    const green = pixels[index + 1];
-                    const blue = pixels[index + 2];
-                    const alpha = pixels[index + 3];
-                    const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
-
-                    if (alpha > 0) {
-                        this.particlesArray.push(new Particle(this, x, y, color));
-                    }
-                }
-
+        init() {
+            for (let i = 0; i < 10000; i++) {
+                this.particlesArray.push(new Particle(this));
             }
+
 
         }
         draw(context) {
-            this.particlesArray.forEach((particle) => particle.draw(context));
+            this.particlesArray.forEach((particle) =>
+                particle.draw(context));
+           // context.drawImage(this.image, this.x, this.y,);
         }
         update() {
-            this.x++;
             this.particlesArray.forEach((particle) =>
                 particle.update());
         }
@@ -73,7 +58,7 @@ window.addEventListener('load', function () {
     }
 
     const effect = new Effect(canvas.width, canvas.height);
-    effect.init(ctx);
+    effect.init();
 
 
     function animate() {
@@ -82,8 +67,8 @@ window.addEventListener('load', function () {
         effect.update();
         requestAnimationFrame(animate);
     }
-
-    //animate();
+    
+    animate();
 
 });
 
